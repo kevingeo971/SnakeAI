91e27f9699da7ee326446c85d5fff509327319c1
kevingeo971 <kevingeo971@gmail.com>
2018-11-15 18:56:24 +0530

    Training data list created

--- mygame.py
+++ mygame.py
@@ -1,22 +1,27 @@
 import random
 import curses
 
-''' Snake RL '''
+''' ---- SNAKE RL ---- '''
 
 #game parameters------------------
-total_games = 20000         
+total_games = 10000      
 max_moves = 500
 frames = 1
-sh, sw = 8,8
-threshold_score = 0
+sh, sw = 15,15
+threshold_score = 7
 #---------------------------------
 
 score_list = []
+training_data = []
 
 for i in range(0,total_games):
 
     score = 0
     moves = 0
+    
+    moves_and_actions_list = []   # [[[sy,sx,fy,fx],[action]] , [[sy,sx,fy,fx],[action]] , [[sy,sx,fy,fx],[action]] ...... ]
+    previous_observation = []     # [sy,sx,fy,fx]
+    temp = []
 
     s = curses.initscr()
     curses.curs_set(0)
@@ -40,9 +45,13 @@ for i in range(0,total_games):
 
     key=0
     while moves<=max_moves:
-    
-        next_key = w.getch()
+        
+        previous_observation = [snake[0][0],snake[0][1],food[0],food[1]]
+        #next_key = w.getch()
         next_key = random.choice([261,260,258,259])
+        
+        temp = [previous_observation,next_key]
+        moves_and_actions_list.append(temp)
 
         key = key if next_key == -1 else next_key
 
@@ -86,6 +95,12 @@ for i in range(0,total_games):
     curses.endwin()
     score_list.append(score)
 
+    if score >= threshold_score:
+        for i in moves_and_actions_list:
+            training_data.append(i)
+
 score_list=sorted(score_list)
-print(score_list[19989:19999])
+print(score_list[total_games-5:total_games-1])
+print(len(training_data))
+print(training_data[0])
 
